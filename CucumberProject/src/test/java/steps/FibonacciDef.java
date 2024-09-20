4e63b90b63679fc7e2889de9bb7bea7345e7a1a0
nhattranb <s3926629@rmit.edu.vn>
2024-09-20 23:25:00 +0700

    modification of BDD

--- CucumberProject/src/test/java/steps/FibonacciDef.java
+++ CucumberProject/src/test/java/steps/FibonacciDef.java
@@ -4,35 +4,38 @@ import io.cucumber.java.en.*;
 import org.example.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Add {
+public class FibonacciDef {
     CalculatorBasic cb = new CalculatorBasic();
     CalculatorAdvanced ca = new CalculatorAdvanced();
 
     public int a, b, choice;
 
     // step definition code
-    @Given("a calculator")
-    public void a_calculator() {
+    @Given("I have a calculator")
+    public void i_have_a_calculator() {
         System.out.println("I have a calculator");
     }
 
-
-    @When("I enter my first number as \\{a}")
+    @When("I enter my first number as \\\\{a}")
     public void iEnterMyFirstNumberAsA() {
         System.out.println("I enter my first number as a");
         a = 5;
     }
 
-
-    @And("I enter my second number as \\{b}")
+    @And("I enter my second number as \\\\{b}")
     public void iEnterMySecondNumberAsB() {
         System.out.println("I enter my second number as b");
         b = 10;
     }
 
-    @And("I enter my choice as \\{choice}")
-    public void iEnterMyChoiceAsChoice() {
-        System.out.println("I enter my choice as choice");
+    @And("I enter my choice as {int}")
+    public void iEnterMyChoiceAs(int arg0) {
         choice = 1;
+    }
+
+    @Then("I should get the Fibonacci of the two numbers")
+    public void i_should_get_the_fibonacci_of_the_two_numbers() {
+        System.out.println("I should get the Fibonacci of the two numbers");
+        assertEquals(55, ca.Fibonacci(a, b));
     }
 }
